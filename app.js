71b1f6e1fe72a49f2c62881a7a5aa2ba52672b0d
James Lipe <james@jameslipe.com>
2021-04-02 19:06:04 -0700

    switching routing

--- app.js
+++ app.js
@@ -22,14 +22,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-
-app.use(express.static('build'))
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
