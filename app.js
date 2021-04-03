be2a8eff1fbd7139c2907d4386eafaee9519381b
James Lipe <james@jameslipe.com>
2021-04-02 19:04:16 -0700

    changed express order

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   })
 
 app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
@@ -30,6 +29,7 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+app.use(express.static('build'))
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
