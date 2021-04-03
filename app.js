5ae8a1cb285bac612facf82b743617dc057dbc30
James Lipe <james@jameslipe.com>
2021-04-02 18:54:09 -0700

    trying to fix heroku; ;

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+})
 
 
 if (process.env.NODE_ENV === 'test') {
